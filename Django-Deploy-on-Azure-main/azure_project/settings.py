9a6ac6907546d18aa29f5bce2036e56b1d4d77a1
Aline <aline_evelyn@msn.com>
2024-09-15 20:07:35 -0300

    av

--- Django-Deploy-on-Azure-main/azure_project/settings.py
+++ Django-Deploy-on-Azure-main/azure_project/settings.py
@@ -1,4 +1,4 @@
-pip install psycopg2 from pathlib import Path # type: ignore
+
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
